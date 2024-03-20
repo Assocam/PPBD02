fdb82bbc49c8a47781c7067ff5d1254460200484
Sydney1592 <marco200811@live.it>
2024-03-20 17:43:18 +0100

    Lavoro

---
+++ _personale/# Scrivi qua il tuo numero.py
@@ -0,0 +1,34 @@
+# Scrivi qua il tuo codice
+# numero = '4155551212'
+# 
+# part1 = '(415)'
+# part2 = numero[3:6]
+# part3 = numero[6:10]
+# print(part1, part2 + '-' + part3)
+
+numero_tel = '41555512121'
+
+if len(numero_tel) != 10:
+    print('il numero inserito non è lungo 10 cifre!')
+    exit()
+
+
+numero_tel = '4155551212'
+
+parte_1 = '415'
+parte_2 = '555'
+parte_3 = '1212'
+
+res_num = f'({parte_1}) {parte_2}-{parte_3}'             
+ 
+print(res_num)
+
+
+
+
+    
+
+
+
+
+
