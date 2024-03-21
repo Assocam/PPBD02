70bb1a0e5b6ba3635ee827b26eb62370ad95a051
ValeGirly <valentinagirlanda@gmail.com>
2024-03-21 14:13:12 +0100

    agg

---
+++ _personale/esercizio_frutti.py
@@ -0,0 +1,7 @@
+fruit1 = 'Mela'
+fruit2 = 'Banana'
+fruit3 = 'Fragola'
+print(fruit1,fruit2,fruit3,sep=', ', end=". Questi sono solo alcuni frutti!\n\n")
+
+print(fruit1,fruit2,fruit3,sep=', ', end='.\n')
+print('Questi sono solo alcuni frutti!')
