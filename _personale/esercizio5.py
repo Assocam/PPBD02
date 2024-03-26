b251e6019a9782999ebf651cc52d36f25ee69c13
Sydney1592 <marco200811@live.it>
2024-03-26 14:32:38 +0100

    nuvi

--- _personale/esercizio5.py
+++ _personale/esercizio5.py
@@ -2,49 +2,57 @@
 
 
 
-
+import os
 
 from esercizio3 import quadrato, rettangolo, triangolo
-nomefile = 'a.txt'
+nomeFile = 'C:\\Users\\marco\\OneDrive\\Desktop\\Lavoro git\\PPBD02\\a.txt'
 
-with open(nomefile, 'r' , encoding = 'utf-8') as fr:
+nomeFileassoluto = os.path.abspath('a.txt')    
+
+with open(nomeFile, 'r' , encoding = 'utf-8') as fr:
     dati = fr.read()
+
 
 compiti = fr.split('\n')
 
-for c in compiti: 
+contatore = 0
+
+dizio = {}
+
+for c in compiti:
+
+    contatore +=1
 
     componenti = c.split(',')
-
+    chiave = contatore
     figura = componenti[0]
 
+
     if figura == 'quadrato':
-        print('\ncalcolo un quadrato')
         lato = int(componenti[1].split('=')[1])
-        print(f'di lato {lato}.')
         p,a = quadrato(lato)
-        print(f'risultato: Area = {a} e perimetro = {p}')
+        dizio[chiave] = {'figura': figura, 'lato': lato, 'area': a, 'perimetro': p}
+       
 
     elif figura == 'rettangolo':
-        print('\ncalcolo un rettangolo')
         base = int(componenti[1].split('=')[1])
         altezza = int(componenti[2].split('=')[1])
-        print(f' che ha base = {base} e altezza = {altezza}')
         p,a = rettangolo(base,altezza)
-        print(f'risultato: Area = {a} e perimetro = {p}')
+        dizio[chiave] = {'figura': figura, 'base': base, 'altezza': altezza, 'area': a, 'perimetro': p}    
+        
 
     elif figura == 'triangolo':
-        print('\ncalcolo un triangolo')
         base = int(componenti[1].split('=')[1])
         altezza = int(componenti[2].split('=')[1])
         l1 = int(componenti[3].split('=')[1])
         l2 = int(componenti[4].split('=')[1])
-        l3 = int(componenti[5].split('=')[1])                                            #componente[4] = 'lato2=50'
+        l3 = int(componenti[5].split('=')[1]) 
+        dizio[chiave] = {'figura': figura, 'base': base, 'altezza': altezza, 'lato1': l1, 'lato2': l2, 'lato3': l3, 
+                         'area': a, 'perimetro': p}
+                                                                                         #componente[4] = 'lato2=50'
                                                                                          #se splitto ('=') ottengo una lista
                                                                                          #'lato2'       elemento 0 della lista
                                                                                          #'50'          elemento 1 della lista
-                                                                                         # poi voglio l'intero dell'elemento 1
-                                                                                         # della lista quindi '50' -> 50
         print(f'che ha base = {base}, altezza = {altezza} e lati{l1}, {l2}, {l3}')
         p,a = triangolo(base,altezza,l1,l2,l3)
         print(f'risultato: Area = {a} e perimetro = {p}')
@@ -53,3 +61,14 @@ for c in compiti:
         print(f'\nho trovato una figura sconosciuta: {figura}\n')
 
 
+    chiave = contatore
+
+    dizio = [chiave] = {}
+    if figura == 'quadrato':
+    elif figura == 'rettangolo':
+    elif figura == 'triangolo':
+       
+    else:
+        dizio[chiave] = {'figura': figura}   
+
+print(dizio)        
