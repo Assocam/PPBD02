a5b61bb1b3715733ed9a06187641528a14de7e65
PPBD02-10 <luca.serpella@yahoo.it>
2024-03-28 17:02:51 +0100

    28_03_2024

---
+++ _personale/Appunti/Enrico/2024_03_28/2024_03_28.py
@@ -0,0 +1,32 @@
+# Esercizio di oggi
+
+# Dobbiamo produrre un file con estensione .csv [comma separed value - (Nome, Cognome, Età, 55)]
+
+# Ha origine negli anni '70/'80 quando i computer si dovevano telefonare (si chiamavano flussi di informazione)
+
+# 1. Fare un file .csv                                     (formato antico)
+# 2. Leggerlo e trasformarlo in un dizionario
+# 3. Salvare il dizionario in formato .json
+# 4. Leggere .json e trasformarlo in un dizionario         (formato moderno)
+
+
+# Il formato JSON serve per essere utilizzato all'interno di "inter process communication", per far parlare più processi.
+# Formati moderni: JSON (Java Script Object Notation), XML
+# Esempio: Smartphone (front end) - Cloud (Back End) ----------------- (vedi quaderno in data in oggetto)
+
+
+# https -> Hyper Text Transfer Protocol Secure                         (come faccio a trasferire l'html)
+# L'Hyper Text è scritto in HTML -> Hyper Text Markup Language         (come faccio una pagina web)
+
+# <-------------------------------------------------------------------------------------------------------------------------->
+
+# Scopo: Leggere un file CSV
+#        Trasformarlo in un dizionario
+#        Salvarlo in un file .JSON
+#        Rileggerlo
+
+
+
+
+
+
