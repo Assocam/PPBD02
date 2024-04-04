dc375ae9719388830a1c88e13bc05a03e371c5b3
PPBD02-10 <luca.serpella@yahoo.it>
2024-04-04 13:40:52 +0200

    .

--- _personale/Untitled-1.py
+++
@@ -1,5 +0,0 @@
-x = float(input("Inserire un numero positivo: "))
-print(f'{x} > 0 and {x} < 100:', x>0 and x<100)
-print(f'{x} > 0 or {x} < 100:', x>0 or x<100)
-print(f'{x} > 0 or {x} > 100:', x>0 and x>100)
-print(f'{x} > 0 and {x} < 100 or x == -1:', x>0 and x<100 or x == -1)
