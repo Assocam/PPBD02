ee65659221099eacf9840a7db1f8cdc2b1ff5790
Marco <162148144+Sydney1592@users.noreply.github.com>
2024-04-11 15:29:24 +0000

    ok

--- _personale/Eserci base.py
+++ _personale/Eserci base.py
@@ -1,10 +1,26 @@
-a = 10
+# Minimo comune multiplo
 
-print(a, type(a))
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
-a = 'ciao come stai?'
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
 
-print(a)
+def lcm_of_list(numbers):
+    if len(numbers) == 0:
+        return None
+    lcm_result = numbers[0]
+    for num in numbers[1:]:
+        lcm_result = lcm(lcm_result, num)
+    return lcm_result
+
+# Esempio di utilizzo:
+lista_numeri = [3, 6, 9, 12, 15]
+print("Lista di numeri:", lista_numeri)
+print("Minimo comune multiplo:", lcm_of_list(lista_numeri))
+
 
 
 
