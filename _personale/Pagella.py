d09906d147fc7829b2cd840d75dc978d7df25b9a
ValeGirly <162148338+ValeGirly@users.noreply.github.com>
2024-04-12 15:30:53 +0000

    agg

---
+++ _personale/Pagella.py
@@ -0,0 +1,11 @@
+nome_alunno = {Mario}
+cognome_alunno = {Rossi}
+classe_alunno = {IIIA}
+
+voti_matematica = {f(voto_1=[86],voto_2=[74],voto_3=[80],voto_4=[83],voto_5=[78])}
+from math sum(voto_1+voto_2+voto_3+voto_4+voto_5)
+print('nome_alunno' + 'cognome_alunno')
+print('classe_alunno')
+print('classe_alunno')
+
+
