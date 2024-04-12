c28f677947fd3bb0fb39f9bbbb19ce8277976966
Marco <162148144+Sydney1592@users.noreply.github.com>
2024-04-12 12:26:34 +0000

    ok

---
+++ _personale/# Minimo comune multiplo.py
@@ -0,0 +1,14 @@
+# Minimo comune multiplo
+from math import lcm
+
+numeri = [3, 5, 6, 30]
+
+
+
+                            # unpacking
+print(lcm(*numeri))                # un oggetto con * vengono scompatatti per ogni argomento singolo
+
+
+
+
+
