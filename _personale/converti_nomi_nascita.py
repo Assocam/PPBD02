b3dd794151ac42986e128275258f9e11a8a70f6b
Luigi Gabriele Ojeda Moran <gigi33@live.it>
2024-04-17 16:10:37 +0200

    Es

--- _personale/converti_nomi_nascita.py
+++ _personale/converti_nomi_nascita.py
@@ -1,23 +1,25 @@
+# Imports
+from datetime import date
+#-Main()
 dati = {}
-with open('C:\\Users\\Gigi3\\Desktop\\PythonP\\PPBD02\\files_esercizi\\nomi_data_nascita.txt', 'r', encoding='utf-8') as  f:
-    file = f
+dict_eta = {}
+anno_corrente = date.today().year
+
+with open('C:\\Users\\Gigi3\\Desktop\\PythonP\\PPBD02\\files_esercizi\\nomi_data_nascita.txt', mode='r', encoding='utf-8') as  f:
+    file = f.readlines()
     for line in file:
         line = line.strip('\n').split(':')
         dati[line[0]] = (line[1])
-dict_eta = {}
 
 for e in dati:
-    eta = 2024 - int(dati[e])
+    eta = anno_corrente - int(dati[e])
     dict_eta[e] = eta
 
-with open('C:\\Users\\Gigi3\\Desktop\\PythonP\\PPBD02\\_personale\\outputs\\nomi_eta.csv', 'w+', encoding='utf-8') as f:
+with open('C:\\Users\\Gigi3\\Desktop\\PythonP\\PPBD02\\_personale\\outputs\\nomi_eta.csv', mode='+w', encoding='utf-8') as f:
     file = f
-    file.write('Nome, Età\n')
+    file.write('Nome, Età, Anno di nascita\n')
     for e in dict_eta:
-        file.write(e)
-        file.write(',')
-        file.write(str(dict_eta[e]))
-        file.write('\n')
+        file.write(f'{e}, {dict_eta[e]}, Nato/a il{dati[e]}\n')
 
 
 
