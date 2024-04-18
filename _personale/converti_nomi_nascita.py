e3f523e2b0bd57fb0e18fb08570f4a0358aa52f8
Luigi Ojeda <gigi33@live.it>
2024-04-18 15:35:15 +0200

    Uoodate es

--- _personale/converti_nomi_nascita.py
+++ _personale/converti_nomi_nascita.py
@@ -5,26 +5,40 @@ dati = {}
 dict_eta = {}
 anno_corrente = date.today().year
 dict_sub = {}
+lines = []
 
-with open('./files_esercizi/nomi_data_nascita.txt', mode='r', encoding='utf-8') as  f:
+filePath = './files_esercizi/nomi_data_nascita.txt'
+with open(filePath, mode='r', encoding='utf-8') as  f:
     file = f.readlines()
     for line in file:
         line = line.strip().split(':')
-        dati[line[0]] = (line[1]) # Dati = {Nome, Anno di nascita}
+        dati[line[0]] = (line[1]) # Dati = {nome, Anno di nascita}
 
 for e in dati:
     eta = anno_corrente - int(dati[e])
-    dict_eta[e] = eta # Dict_eta = {Nome, età}
-    if eta in dict_sub:
-        dict_sub[eta] = f'{dict_sub[eta]}; {e}'
+    dict_eta[e] = eta # Dict_eta = {nome, età}
+    if eta in dict_sub: # Dict_sub = {età, nomi di persone}
+        dict_sub[eta] = f'{dict_sub[eta]}, {e}'
     else:
         dict_sub[eta] = e
 
-with open('./_personale/outputs/nomi_eta.csv', mode='+w', encoding='utf-8') as f:
+filePath = './_personale/outputs/nomi_eta.csv'
+with open(filePath, mode='+w', encoding='utf-8') as f:
     file = f
-    file.write('Nome, Età, Anno di nascita\n')
-    for e in dict_eta:
-        file.write(f'{e}, {dict_eta[e]}, Nato/a il{dati[e]}\n')
-    file.write('\nEtà, Persone\n')
-    for e in dict_sub:
-        file.write(f'{e}, {dict_sub[e]}\n')
+    lines.append('Nome, Età, Anno di nascita\n')
+    for e in sorted(dict_eta):
+        lines.append(f'{e}, {dict_eta[e]}, Nato/a il{dati[e]}\n')
+    lines.append('\n')
+    file.writelines(lines)
+
+lines = []
+
+filePath = './_personale/outputs/eta_nomi.txt'
+with open(filePath, mode='+w', encoding='utf-8') as f:
+    file = f
+    lines.append('Età: Persone\n')
+    for e in sorted(dict_sub):
+        lines.append(f'{e}: {dict_sub[e]}\n')
+    lines.append('\n')
+    file.writelines(lines)
+    
