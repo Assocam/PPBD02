322e4bdb088fc634b0e84b0507611984b8332cce
Luigi Ojeda <gigi33@live.it>
2024-04-18 17:30:26 +0200

    Update es

--- _personale/converti_nomi_nascita.py
+++ _personale/converti_nomi_nascita.py
@@ -1,44 +1,48 @@
 # Imports
 from datetime import date
+import sys
+args = sys.argv
 #-Main()
-dati = {}
-dict_eta = {}
 anno_corrente = date.today().year
-dict_sub = {}
-lines = []
+dati = {} # dict = {nome, anno di nascita}
+dict_eta = {} # dict = {nome, eta}
+dict_sub = {} # dict = {eta, nomi di persone con quell'età}
+output_file = [] # lista di line da scrivere nei nuovi files
+
+input_path = args [1]
+output_path1 = args[2]
+output_path2 = args[3]
 
 filePath = './files_esercizi/nomi_data_nascita.txt'
-with open(filePath, mode='r', encoding='utf-8') as  f:
+with open(input_path, mode='r', encoding='utf-8') as  f:
     file = f.readlines()
     for line in file:
         line = line.strip().split(':')
-        dati[line[0]] = (line[1]) # Dati = {nome, Anno di nascita}
+        dati[line[0]] = (line[1])
 
 for e in dati:
     eta = anno_corrente - int(dati[e])
-    dict_eta[e] = eta # Dict_eta = {nome, età}
-    if eta in dict_sub: # Dict_sub = {età, nomi di persone}
+    dict_eta[e] = eta 
+    if eta in dict_sub: 
         dict_sub[eta] = f'{dict_sub[eta]}, {e}'
     else:
         dict_sub[eta] = e
 
 filePath = './_personale/outputs/nomi_eta.csv'
-with open(filePath, mode='+w', encoding='utf-8') as f:
+with open(output_path1, mode='+w', encoding='utf-8') as f:
     file = f
-    lines.append('Nome, Età, Anno di nascita\n')
+    output_file.append('Nome, Età, Anno di nascita:\n')
     for e in sorted(dict_eta):
-        lines.append(f'{e}, {dict_eta[e]}, Nato/a il{dati[e]}\n')
-    lines.append('\n')
-    file.writelines(lines)
+        output_file.append(f'{e}, {dict_eta[e]}, Nato/a il{dati[e]}\n')
+    output_file.append('\n')
+    file.writelines(output_file)
 
-lines = []
+output_file = []
 
 filePath = './_personale/outputs/eta_nomi.txt'
-with open(filePath, mode='+w', encoding='utf-8') as f:
+with open(output_path2, mode='+w', encoding='utf-8') as f:
     file = f
-    lines.append('Età: Persone\n')
+    file.write('Età: Persone\n')
     for e in sorted(dict_sub):
-        lines.append(f'{e}: {dict_sub[e]}\n')
-    lines.append('\n')
-    file.writelines(lines)
-    
+        file.write(f'{e}: {dict_sub[e]}\n')
+    file.write('\n')
---
+++ _personale/moltiplica_due_numeri.py
@@ -0,0 +1,11 @@
+import sys
+
+
+args = sys.argv
+
+primo_num = float(args[1])
+secondo_num = float(args[2])
+
+risultato = primo_num * secondo_num
+
+print(f'Il prodotto di {primo_num} e {secondo_num} è: {risultato}\n')
