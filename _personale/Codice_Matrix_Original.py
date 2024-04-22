b2b9e6a2542b1db2c6cf7276b61f2e249439f417
Sydney1592 <marco200811@live.it>
2024-04-22 16:03:24 +0200

    nuovi

--- _personale/Codice_Matrix_Original.py
+++ _personale/Codice_Matrix_Original.py
@@ -1,20 +1,30 @@
 from tkinter import Frame, Tk, Canvas
-from random import choice
-from font_tools import get_font_chars
+from random import choice 
 
 class Matrix(Frame):
-	def __init__(self, master, font_name, font_size):
+	def __init__(self, master):
 		super().__init__(master)
 
-		self.canvas = Canvas(master, bg='black')
+		self.canvas = Canvas(master, bg= 'black')
 		self.canvas.place(relx=0, rely=0, relwidth=1, relheight=1)
+
 		self.velocidad = [i for i in range(0,30,5)]
 		self.pos = [i for i in range(-200,200,20)]
 		self.letters = []
 		self.green = 0
-		self.font_name = font_name
-		self.font_size = font_size
-		self.characters = get_font_chars(font_name)
+		self.caracteres = [
+                      "a", "k", "u", "u", "u", "И", "Ч",
+                      "b", "l", "v", "v", "v", "Ѳ", "Р",
+                      "c", "m", "w", "w", "w", "І", "С",
+                      "d", "n", "x", "x", "x", "К", "Т",
+                      "e", "o", "y", "y", "y", "Л", "Ѵ",
+                      "f", "p", "z", "z", "z", "М", "Ф",
+                      "g", "q", "1", "1", "1", "Н", "Х",
+                      "h", "r", "2", "2", "2", "Ѯ", "Ѱ",
+                      "i", "s", "3", "3", "3", "Ѻ", "Ѿ",
+                      "j", "t", "4", "4", "4", "П", "Ц",
+                ]
+
 		self.draw()
 		self.update()
 
@@ -22,13 +32,9 @@ class Matrix(Frame):
 		for x in range(0,1600,20):
 			y = choice(self.pos)
 			for j in range(0, choice([180,220,280]),20):
-				self.obj = self.canvas.create_text(
-					20+x, -200+y+j,  # posizione x, y
-					text=choice(self.characters),
-					fill='green2',
-					font=(self.font_name, self.font_size))
+				self.obj = self.canvas.create_text(20+x, -200+y+j, text= choice(self.caracteres),
+					fill = 'green2', font= ('Arial', 14))
 				self.letters.append(self.obj)
-	
 	def update(self):
 		for letter in self.letters:
 			v = choice(self.velocidad)
@@ -50,11 +56,10 @@ class Matrix(Frame):
 		
 		self.canvas.after(80, self.update)
 
-
 if __name__ == '__main__':
 	root = Tk()
 	root.title('Matrix Animation')
 	root.config(bg= 'black')
 	#root.attributes('-fullscreen', True)
-	app = Matrix(root, 'Arial Unicode', 14)
+	app = Matrix(root)
 	app.mainloop()
